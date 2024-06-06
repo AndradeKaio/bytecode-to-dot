081063049af77214c17ba00cc5204aaad643454f
Kaio Henrique <deandradekaio@gmail.com>
2024-06-06 12:06:50 -0300

    feat: print instructions and build arrows

--- BytecodeDot.cpp
+++ BytecodeDot.cpp
@@ -1,7 +1,9 @@
-#include "llvm/IR/BasicBlock.h"
+#include <fstream>
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/LegacyPassManager.h"
+#include "llvm/IR/Instructions.h"
 #include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
@@ -14,18 +16,49 @@ namespace {
     static char ID;
     BytecodeDot(): FunctionPass(ID){}
 
-    bool runOnFunction(Function &F) override {
-      errs() << "Function name: ";
-      errs().write_escaped(F.getName()) << '\n';
-      for(BasicBlock &BB: F) {
-        errs() << "BasicBlock: " << BB.getName() << "\n";
-        for (Instruction &I : BB) {
-          errs() << I << "\n";
-        }
-      }
-      return false;
+    std::string get_block_reference(BasicBlock *BB){
+      std::string block_address;
+      raw_string_ostream string_stream(block_address);
+      BB->printAsOperand(string_stream, false);
+      return string_stream.str().substr(1);
     }
 
+    bool runOnFunction(Function &F) override {
+      std::string funcName = F.getName().str();
+      std::ofstream file(funcName + ".dot");
+      file << "digraph \"CFG of " + funcName +  " function\"{\n";
+
+      errs() << "Function name: ";
+      errs().write_escaped(funcName) << '\n';
+
+      for(BasicBlock &BB: F) {
+        errs() << "BasicBlock: ";
+        auto bb_name = get_block_reference(&BB);
+        file << "\tBB" + bb_name + " [shape=record,label=\"{BB" + bb_name + "\n";
+        std::string str;
+        raw_string_ostream so(str);
+        for (Instruction &I : BB) {
+          so << I;
+          file << "\t " + so.str() << "\\n";
+          str.clear();
+          // if(I.isterminator()) {
+          //   
+          // }
+        }
+        file << "\t}\"];\n";
+        Instruction *terminator = BB.getTerminator();
+        if(terminator){
+          for(unsigned int i=0; i<terminator->getNumSuccessors(); i++){
+            BasicBlock *succ = terminator->getSuccessor(i);     
+            auto succ_name = get_block_reference(succ);
+            file << "\tBB"+ bb_name + "-> BB" + succ_name + "\n";
+          }
+        }
+      }
+      file << "}\n";
+      file.close();
+      return false;
+    }
   };
 }
 
